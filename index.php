9fa136479a419104e69dbcf0feff749ebf02e3d3
fwsGonzo <fwsgonzo@hotmail.com>
2013-09-02 16:19:26 +0200

    HTML5 test side

--- index.php
+++ index.php
@@ -1,8 +1,39 @@
-<?php include 'site_top.php'; />
-
-<?php include 'menu.php'; />
-
-
-<-- stuff />
-
-<?php include 'site_bottom.php'; />
+<!doctype html>
+<html lang="no">
+<head>
+  <meta charset="UTF-8">
+  <title>Web prosjekt</title>
+</head>
+<body>
+  <h1>Web prosjekt forside</h1>
+  
+  <h2>Stuff</h2>
+  <ul>
+    <li>Punkt 1</li>
+    <li>Punkt 2</li>
+  </ul>
+  
+  <ol>
+    <li><strong>Stuff</strong> with more stuff. 
+      <p>Mucho text</p>
+      <ul>
+        <li>Stuff</li>
+      </ul>
+    </li>
+    <li>
+	  <p><strong>Nice pictures</strong></p>
+      <p>Text</p>
+      <ul>
+        <li>Stuff
+          <ul>
+            <li>Stuff 1</li>
+            <li>Stuff 2</li>
+            <li><a href="http://en.wikipedia.org/">Stuff 3</a></li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+  </ol>
+  
+</body>
+</html>
