8c8a5231c984a2ff4bd3eb5ae404a63d56817155
Svein Gunnar Fagerheim <sveinfagerheim@gmail.com>
2013-10-29 10:46:02 +0100

    Added sections

--- page-main.php
+++ page-main.php
@@ -1,23 +1,6 @@
-<h2>Om siden</h2>
-<p>
-	Siden ble laget som følge av obligatorisk oppgave i studie-emnet Web-Prosjekt.
-</p>
-<p>
-	Og, siden jeg absolutt må ha noe video, eller audio med:
-</p>
 
-<h3>
-	Audio-snutt:
-</h3>
-<ul>
-	<li>
-		<audio controls="controls">
-			<source src="http://art.fwsnet.net/kevin_pwned.wav" type="audio/wav" />
-			Webleseren din støtter ikke HTML5 / audio tag!
-		</audio>
-	</li>
-</ul>
-<p>
-	En liten snutt fra en gammel voice chat.
-</p>
+<div id="leftSection"></div>
 
+<div id="middleSection"></div>
+
+<div id="rightSection"></div>
