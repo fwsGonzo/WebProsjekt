21414e251d2541ffecaa82ac46159f1870f7cb8f
Gonzo <fwsgonzo@hotmail.com>
2013-10-29 12:25:56 +0100

    Fixed keyboard.php

--- keyboard.php
+++ keyboard.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 <img id="keyboard" src="keyboard.jpg" alt="~ Keyboard ~" />
 
 <script src="keyboard.js" type="text/javascript">
 </script>
-=======
-﻿	
->>>>>>> f206f1f06e15c4167115ea9a361aa4d4f34bcc46
