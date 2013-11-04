dd30afe969a6ecb27c8cac5b13ba318c84245d35
Svein Gunnar Fagerheim <sveinfagerheim@gmail.com>
2013-11-04 17:21:42 +0100

    Done some small changes

--- footer.php
+++ footer.php
@@ -3,7 +3,8 @@
 		
 		<footer class="inparagraph" style="clear:both;" >
 			Gruppe 7, november 2013.
-
+			</br>
+			<a href="http://validator.w3.org/check?uri=referer"><img src="validhtml5.png" alt="Bilde av W3 html valideringsikon"></a>
 		</footer>	
 	</section>
 	
