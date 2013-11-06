c78f5d1d7e4878be8c7c2bc0db6c7a3cc4cbab77
Mateusz Zych <mateusz42@hotmail.com>
2013-11-06 12:46:43 +0100

    Reset and save button in menu

--- menu.php
+++ menu.php
@@ -1,6 +1,10 @@
 <section id="menu">
 	<h2>Menu</h2>
-	<button>Reset</button>
-	<p>reset</p>
+	<p>Restart game </p>
+        <button class="menuButton" id="resetGame">Reset</button>
+        
+        <p>Save game </p>
+        <button class="menuButton" id="saveGame">Save</button>
+	
 	
 </section>
