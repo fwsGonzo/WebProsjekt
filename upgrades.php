a804dc3ffcec8e5516218b44c4af0d30e16931fe
Svein Gunnar Fagerheim <sveinfagerheim@gmail.com>
2013-11-07 20:35:47 +0100

    divided rightsection into to divs

--- upgrades.php
+++ upgrades.php
@@ -1,26 +1,12 @@
-<?php
-	/*
-	for($i=0; $i<10; $i++)
-	{
-		echo "<div class=\"updatesOption\">";
-			//Imagecontainer
-			echo "<div class=\"icon\">";
-				echo "<img src=\"programming.png\" alt=\"Icon of something\"/>";
-			echo "</div>";
-			
-			echo "<div class=\"name\">Navn</div>";
-			echo "<div class=\"price\">Price</div>";
-			echo "<div class=\"ownedUnits\">owned</div>";
-			
-			//echo "haha";
-		echo "</div>";
-	}
-	*/
-?>
-<script src="buildings.js" type="text/javascript">
-</script>
-<script src="upgrades.js" type="text/javascript">
-</script>
-<script src="hover.js" type="text/javascript">
-</script>
 
+<div class="rightSection" id="rightSectionPart1">
+	<script src="buildings.js" type="text/javascript">
+	</script>
+	<script src="upgrades.js" type="text/javascript">
+	</script>
+	<script src="hover.js" type="text/javascript">
+	</script>
+</div>
+<div class="rightSection" id="rightSectionPart2">
+	
+</div>
