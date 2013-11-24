7cd59fd8fd928c8481bf35f671112bee3d1f0803
Svein Gunnar Fagerheim <sveinfagerheim@gmail.com>
2013-11-24 12:37:52 +0100

    Fixed error so it pass the validator test

--- menu.php
+++ menu.php
@@ -1,9 +1,7 @@
 <section id="menu">
 	<h2>Menu</h2>
-	<center>
-		<a href=""><button class="menuButton" id="resetThisGame" onclick="resetThisGame()">Reset this game</button></a>
+	<button class="menuButton" id="resetThisGame" onclick="resetThisGame()">Reset this game</button>
 	
-        <a href=""><button class="menuButton" id="resetGame" onclick="reset()">Reset all game</button><a>
-        </center>
-	
+    <button class="menuButton" id="resetGame" onclick="reset()">Reset all game</button>
+
 </section>
